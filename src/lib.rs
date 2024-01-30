70dc97bd37047631628e1f87d32e8a08155621d2
PawelBis <bis1pawel1@gmail.com>
2024-01-30 23:53:03 +0100

    Replace unwraps with error variants

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use bevy::{
     utils::{self, HashMap},
 };
 use rusqlite::Connection;
-use std::{env, path::Path, sync::Mutex};
+use std::{env, sync::Mutex};
 use thiserror::Error;
 use walkdir::WalkDir;
 
@@ -15,7 +15,13 @@ enum Error {
     #[error("failed to read CARGO_MANIFEST_DIR env var")]
     MissingCargoManifestDir(#[from] std::env::VarError),
     #[error("failed to open SQLite connection")]
-    FailedToOpenSqliteConnection(#[from] rusqlite::Error),
+    DbError(#[from] rusqlite::Error),
+    #[error("parent directory missing")]
+    ParentDirectoryMissing,
+    #[error("filename missing")]
+    FilenameMissing,
+    #[error("failed 'to_str()'")]
+    FiledToStrConversion,
 }
 
 #[derive(Resource, Debug)]
@@ -60,14 +66,14 @@ fn setup_db(mut commands: Commands) -> Result<(), Error> {
         (),
     )?;
 
-    scan_fs_for_assets(&db, assets_dir);
+    scan_fs_for_assets(&db, assets_dir)?;
 
     commands.insert_resource(DbConnection(Mutex::new(db)));
     Ok(())
 }
 
 /// Populate `asset` table with assets found in file system
-fn scan_fs_for_assets(db: &Connection, assets_dir: String) {
+fn scan_fs_for_assets(db: &Connection, assets_dir: String) -> Result<(), Error> {
     let mut dir_to_id = HashMap::<String, u32>::new();
     let mut current_id: u32 = 1;
     for entry in WalkDir::new(assets_dir) {
@@ -76,24 +82,31 @@ fn scan_fs_for_assets(db: &Connection, assets_dir: String) {
         if entry_path.is_dir() {
             let parent_name = entry_path
                 .parent()
-                .unwrap()
+                .ok_or(Error::ParentDirectoryMissing)?
                 .file_name()
-                .unwrap()
+                .ok_or(Error::FilenameMissing)?
                 .to_str()
-                .unwrap()
+                .ok_or(Error::FiledToStrConversion)?
                 .to_owned();
             let parent_id = dir_to_id.get(&parent_name);
 
-            let inserted_rows = db
-                .execute(
-                    "INSERT INTO directory (name, parent) VALUES(?1, ?2);",
-                    (entry.file_name().to_str(), parent_id),
-                )
-                .unwrap();
+            let inserted_rows = db.execute(
+                "INSERT INTO directory (name, parent) VALUES(?1, ?2);",
+                (entry.file_name().to_str(), parent_id),
+            )?;
 
-            dir_to_id.insert(entry.file_name().to_str().unwrap().to_owned(), current_id);
+            dir_to_id.insert(
+                entry
+                    .file_name()
+                    .to_str()
+                    .ok_or(Error::FiledToStrConversion)?
+                    .to_owned(),
+                current_id,
+            );
             current_id += inserted_rows as u32;
         } else {
         }
     }
+
+    Ok(())
 }
